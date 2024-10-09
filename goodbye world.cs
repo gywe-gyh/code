546c82e21937498e3ae8f218832dd12d2e7479b0
gywe-gyh <ivan.maks0130@gmail.com>
2024-10-09 14:34:15 +0500

    Add files via upload

---
+++ goodbye world.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goodbyeworld : MonoBehaviour
+{
+    public Vector3 abeme;
+
+    public int health = 5;
+    public int level = 2;
+    public float speed = 1.2f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       transform.position=transform.position + abeme;
+        
+
+    }
+}
